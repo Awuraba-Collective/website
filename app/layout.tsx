f69a72e2df9e0296552e1fab25a0502d8cfbf68c
Frederick Obeng-Nyarko <nernuer@gmail.com>
2026-01-10 20:12:12 +0000

    feat: add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { WhatsAppButton } from "@/components/WhatsAppButton";
 import StoreProvider from "@/store/StoreProvider";
 import { Toaster } from "sonner";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -124,6 +125,7 @@ export default function RootLayout({
           <Toaster position="top-right" richColors />
         </StoreProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
